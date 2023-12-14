1635eae94b34bd89ba43d887f075798dd2816066
lwx <lwx2002@163.com>
2023-12-14 19:51:26 +0800

    auto kill thread

--- src/main/java/org/example/codeGenerator/serivce/GeneratorService.java
+++ src/main/java/org/example/codeGenerator/serivce/GeneratorService.java
@@ -1,10 +1,13 @@
 package org.example.codeGenerator.serivce;
 
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.example.codeGenerator.config.BaseConfig;
 import org.example.codeGenerator.config.GeraltorConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
 
 @Service
 @Slf4j
@@ -19,6 +22,10 @@ public class GeneratorService {
     @Autowired
     Generator generator;
 
+    @Resource
+    private WebApplicationContext applicationContext;
+
+
     public void genralCode() {
         String outdir = geraltorConfig.getValue("outputDir");
         log.info("genralCode start ，output dir: {}", outdir);
@@ -28,5 +35,6 @@ public class GeneratorService {
             log.error("genralCode error：", e);
         }
         log.info("genralCode finish ，output dir:{}", outdir);
+        System.exit(SpringApplication.exit(applicationContext));
     }
 }
